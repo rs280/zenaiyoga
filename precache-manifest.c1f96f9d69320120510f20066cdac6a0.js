f9cfdfdbb7467f8c306f2ee9b6d0e486d9389c62
rs280 <rishabhsahoo1@gmail.com>
2023-01-08 13:40:11 -0800

    Updates

---
+++ precache-manifest.c1f96f9d69320120510f20066cdac6a0.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd76535da066268da1da4e5b2b3bad0f",
+    "url": "/zenaiyoga/index.html"
+  },
+  {
+    "revision": "7eaef9e6fe6cd17e1ef6",
+    "url": "/zenaiyoga/static/css/main.c062beca.chunk.css"
+  },
+  {
+    "revision": "1a09ba6c2b5a97733252",
+    "url": "/zenaiyoga/static/js/2.51906d74.chunk.js"
+  },
+  {
+    "revision": "376f279d06e0f7c640337d5d8f7cdffa",
+    "url": "/zenaiyoga/static/js/2.51906d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eaef9e6fe6cd17e1ef6",
+    "url": "/zenaiyoga/static/js/main.10a304bd.chunk.js"
+  },
+  {
+    "revision": "3d5b754087f1b73384ba",
+    "url": "/zenaiyoga/static/js/runtime-main.f3f706b1.js"
+  },
+  {
+    "revision": "5323d604d711bf77f59b400acb223fce",
+    "url": "/zenaiyoga/static/media/00000212.5323d604.jpg"
+  },
+  {
+    "revision": "5fe9fd5dfe8cf348c4cb137049a37c15",
+    "url": "/zenaiyoga/static/media/Green_tick.5fe9fd5d.png"
+  },
+  {
+    "revision": "6d22ac96dfea52015284d45c266b9f37",
+    "url": "/zenaiyoga/static/media/Keypoints.6d22ac96.png"
+  },
+  {
+    "revision": "f49f8d438b6e5d083502746a96953aa0",
+    "url": "/zenaiyoga/static/media/Posenet model.f49f8d43.png"
+  },
+  {
+    "revision": "e41f70fed2fafae471d01866011ce215",
+    "url": "/zenaiyoga/static/media/Shirshasana.e41f70fe.png"
+  },
+  {
+    "revision": "b08d6f35d2cb9517f7a353a99370bc5c",
+    "url": "/zenaiyoga/static/media/a.b08d6f35.jpg"
+  },
+  {
+    "revision": "8e6aefe136dc857874c2a94ef37297b5",
+    "url": "/zenaiyoga/static/media/boy.8e6aefe1.jpg"
+  },
+  {
+    "revision": "b3f8c3cfd124fbb0d24d207789939ec3",
+    "url": "/zenaiyoga/static/media/cardchairpose.b3f8c3cf.jpg"
+  },
+  {
+    "revision": "d2fc0e21941c0a12f53ed9ac09b6353d",
+    "url": "/zenaiyoga/static/media/cardgoddess.d2fc0e21.jpg"
+  },
+  {
+    "revision": "d7b2890a2b594e2e361732f2d8ff7e90",
+    "url": "/zenaiyoga/static/media/cardmeditation.d7b2890a.jpg"
+  },
+  {
+    "revision": "df77bf5f0d0ca0a037abca88244c77ce",
+    "url": "/zenaiyoga/static/media/cardplank.df77bf5f.webp"
+  },
+  {
+    "revision": "d198060012555e6eef2a57cfc51dcae4",
+    "url": "/zenaiyoga/static/media/cardsurya.d1980600.jpg"
+  },
+  {
+    "revision": "1a364d8688a786867a107e8df0c92a1b",
+    "url": "/zenaiyoga/static/media/cardtadasana.1a364d86.jpg"
+  },
+  {
+    "revision": "592a6a093be960e3ba9c6fd1b20073fd",
+    "url": "/zenaiyoga/static/media/cardtree.592a6a09.jpg"
+  },
+  {
+    "revision": "28b604ab38392ce0e16e9f1ce094c063",
+    "url": "/zenaiyoga/static/media/cardwarrior.28b604ab.jpg"
+  },
+  {
+    "revision": "d7d556209b1d3eb58dcb97a316eec9ef",
+    "url": "/zenaiyoga/static/media/cross.d7d55620.png"
+  },
+  {
+    "revision": "9e2d88e245e7d9e66891f4b19bd1406d",
+    "url": "/zenaiyoga/static/media/crossed.9e2d88e2.webp"
+  },
+  {
+    "revision": "0d8406f54b1dcbacf435c6e07631890f",
+    "url": "/zenaiyoga/static/media/forward-bend.0d8406f5.webp"
+  },
+  {
+    "revision": "0ca639c04bcd3a6107376d51e8599122",
+    "url": "/zenaiyoga/static/media/goddess.0ca639c0.jpg"
+  },
+  {
+    "revision": "3dc0717308508792843315ed04da2c0c",
+    "url": "/zenaiyoga/static/media/hand.3dc07173.jpg"
+  },
+  {
+    "revision": "8e546d100c14b2c2c3fa5b138b24bb17",
+    "url": "/zenaiyoga/static/media/handraise.8e546d10.jpg"
+  },
+  {
+    "revision": "afa9bc31db603b4e16cd169c3040a2bd",
+    "url": "/zenaiyoga/static/media/loading.afa9bc31.svg"
+  },
+  {
+    "revision": "891da7b2d99a808fce146f1868649624",
+    "url": "/zenaiyoga/static/media/man.891da7b2.jpg"
+  },
+  {
+    "revision": "0820c1a34b17a97d02261ba8ea2a5a00",
+    "url": "/zenaiyoga/static/media/meditation.0820c1a3.jpg"
+  },
+  {
+    "revision": "a6a323360f9c7ae79e1eb2bd8266a0b2",
+    "url": "/zenaiyoga/static/media/plank.a6a32336.png"
+  },
+  {
+    "revision": "af9c3fd196e27e19970f254115412f6c",
+    "url": "/zenaiyoga/static/media/sirsasana.af9c3fd1.jpg"
+  },
+  {
+    "revision": "9f203438a599d677a649250f9ea40691",
+    "url": "/zenaiyoga/static/media/sit.9f203438.jpg"
+  },
+  {
+    "revision": "bcfefd302e92902ac5be925ff6374e53",
+    "url": "/zenaiyoga/static/media/sitting.bcfefd30.webp"
+  },
+  {
+    "revision": "414515aa54f215c7161c386e4daf776e",
+    "url": "/zenaiyoga/static/media/squat.414515aa.jpg"
+  },
+  {
+    "revision": "c41e1a2fcc92926ca3d0925c2a1e7bf8",
+    "url": "/zenaiyoga/static/media/suryanamskar.c41e1a2f.jpg"
+  },
+  {
+    "revision": "0d7cb44a263d709f497e1e79e54183de",
+    "url": "/zenaiyoga/static/media/tadasana.0d7cb44a.jpg"
+  },
+  {
+    "revision": "3e7591dc9943713ffb97035423a266ae",
+    "url": "/zenaiyoga/static/media/tree (2).3e7591dc.jpg"
+  },
+  {
+    "revision": "365ab565e565d098e4a37cc5e3ecea72",
+    "url": "/zenaiyoga/static/media/warrior.365ab565.jpg"
+  },
+  {
+    "revision": "b7f050e46aa0ae8c7d7faa729caa605f",
+    "url": "/zenaiyoga/static/media/yoga.b7f050e4.jpg"
+  }
+]);
